1e1dae0455383b0fa3890ffde9d38212f824c531
hieu1012 <daotao@gmail.com>
2025-02-26 02:28:52 +0700

    feat: update header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { FooterComponent } from './shared/Footer/footer.component';
+import { HeaderComponent } from './shared/Header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

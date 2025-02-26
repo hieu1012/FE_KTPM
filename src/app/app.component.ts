95245d27c8ad79b3b7f3f46f878c5f41ba3e809e
hieu1012 <daotao@gmail.com>
2025-02-26 16:11:58 +0700

    feat: add footer and update header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import { FooterComponent } from './shared/Footer/footer.component';
+import { FooterComponent } from './shared/Footer/footer.component';
 import { HeaderComponent } from './shared/Header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
